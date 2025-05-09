123b842be4f7a0d8e4d095f60eea1d4625cbea2a
francmarra <marracho@sapo.pt>
2025-05-09 09:48:59 +0100

    editar UC

--- Controllers/UCController.cs
+++ Controllers/UCController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Edveeeeeee.Data;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Edveeeeeee.Models;
 
 namespace Edveeeeeee.Controllers
 {
@@ -27,6 +29,39 @@ namespace Edveeeeeee.Controllers
                 .ToList();
 
             return View(ucs);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UnidadeCurricular uc)
+        {
+            var userId = HttpContext.Session.GetInt32("userId");
+            if (userId == null) return RedirectToAction("Login", "Conta");
+
+            uc.ProfessorId = (int)userId;
+            _context.UCs.Add(uc);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var uc = _context.UCs.FirstOrDefault(x => x.Id == id);
+            if (uc == null) return NotFound();
+            return View(uc);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(UnidadeCurricular uc)
+        {
+            _context.UCs.Update(uc);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
