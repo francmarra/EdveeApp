0c0ba767b3831bc5c5112eb9ae223b3251c6a226
francmarra <marracho@sapo.pt>
2025-05-19 19:06:53 +0100

    ligacoes com copilot

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,29 @@
 using System.Diagnostics;
 using Edveeeeeee.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Edveeeeeee.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Connections()
+        {
+            // Example data - adapt to your needs. Cells are string IDs, connections are lists of cell indices
+            var cells = new List<string> { "1", "2", "3" };
+            var connections = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 2, 3, 1 },
+                new List<int> { 3, 1, 2 }
+            };
+
+            ViewBag.Cells = cells;
+            ViewBag.Connections = connections;
+
+            return View();
+        }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
