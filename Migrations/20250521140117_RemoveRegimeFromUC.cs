294a9eaf2ae753dca5b2dd52bbad63c040596b86
ilordan <76077474+ilordan@users.noreply.github.com>
2025-05-21 15:05:43 +0100

    ja funciona

---
+++ Migrations/20250521140117_RemoveRegimeFromUC.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edveeeeeee.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRegimeFromUC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Atividades",
+                table: "UCs");
+
+            migrationBuilder.DropColumn(
+                name: "Avaliacao",
+                table: "UCs");
+
+            migrationBuilder.DropColumn(
+                name: "Competencias",
+                table: "UCs");
+
+            migrationBuilder.DropColumn(
+                name: "Conteudos",
+                table: "UCs");
+
+            migrationBuilder.DropColumn(
+                name: "Regime",
+                table: "UCs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Atividades",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Avaliacao",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Competencias",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Conteudos",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Regime",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
